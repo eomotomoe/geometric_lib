b68c0b4ae4362a05fa18656157c672e72380b628
Mansur Satbanov <smansupera@gmail.com>
2024-10-04 17:18:40 +0300

    Была добавлена документация

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    """ Принимает число r(радиус круга), возвращает квадрат r умноженный на pi """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """ Принимает число r(радиус круга), возвращает r умноженный на два pi """
     return 2 * math.pi * r
 
